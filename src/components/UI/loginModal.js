6a433ece1e9946c52c25998e25d109b6f4e0d84a
Adam Pugh <adampugh@zoho.com>
2018-02-06 15:19:12 +0000

    Completed log in modal

--- src/components/UI/loginModal.js
+++ src/components/UI/loginModal.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Modal from "react-modal";
 
+import GoogleIcon from "../../assets/photos/googlelogo.jpg";
+import FacebookIcon from "../../assets/photos/facebooklogo.jpg";
+
 const customStyles = {
     overlay : {
-        zIndex: 200,
+        zIndex: 2000,
         // display: 'flex',
         // alignItems: "center",
         // justifyContent: "center"
+        background: "rgba(0, 0, 0, 0.6)"
       },
       content : {
       }
@@ -19,14 +23,31 @@ const loginModal = (props) => (
         appElement={document.getElementById('root')} 
         onRequestClose={props.handleCloseModal}
         style={customStyles}
-        closeTimeoutMS={200}  
+        closeTimeoutMS={200}
+        className="modal"
     >
         <div className="loginModal">
-            <h3 className="loginModal__close">
-                <span onClick={props.handleCloseModal}>
-                    <i className="fas fa-times"></i>
-                </span>
-            </h3>
+            <div className="loginHeader">
+                <div></div>
+                <div><h3>Log In</h3></div>
+                <div className="loginModal__close">
+                    <h3>
+                        <span onClick={props.handleCloseModal}>
+                            <i className="fas fa-times"></i>
+                        </span>
+                    </h3>
+                </div>
+            </div>
+            <hr />
+
+            <button className="loginModal__button">
+                <img src={GoogleIcon} alt="google logo" />
+                <span>Sign in with Google</span>
+            </button>
+            <button className="loginModal__button">
+                <img src={FacebookIcon} alt="facebook logo" />
+                <span>Sign in with Facebook</span>
+            </button>
         </div>
     </Modal>
 );
