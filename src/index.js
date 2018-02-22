13910d2514847a208438e458d6a6b0e5e6129c2c
Adam Pugh <adampugh@zoho.com>
2018-02-22 21:47:01 +0000

    Added redux devtools

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import './styles/styles.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from "./store/reducers/reducer";
 
-const store = createStore(reducer);
+const store = createStore(reducer, /* preloadedState, */ devToolsEnhancer());
 
 const app = (
     <Provider store={store}>
